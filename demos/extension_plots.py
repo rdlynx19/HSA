93996c52935d7973aacaf5cd0100f4294855fc4d
rdlynx19 <pushkardave.vnit@gmail.com>
2025-10-22 17:22:29 -0500

    finished plotting

--- demos/extension_plots.py
+++ demos/extension_plots.py
@@ -13,20 +13,20 @@ def main():
     sim.enable_actuator_group(1)
 
     try:
-        # sim.position_control_crawl(position=2.8, lock=False, plot=True)
+        sim.position_control_crawl(position=2.8, lock=True, plot=True)
         # sim.position_control_bend_left(position=2.8, duration=2.5, plot=True)
         # sim.position_control_contraction(duration=2.5, plot=True)
         # sim.position_control_bend_right(position=2.8, duration=2.5, plot=True)
         # sim.position_control_contraction(duration=2.5, plot=True)
-        sim.position_control_twist1(position=2.8, duration=2.5, plot=True)
-        sim.position_control_contraction(duration=2.5, plot=True)
-        sim.position_control_twist2(position=2.8, duration=2.5, plot=True)
-        sim.position_control_contraction(duration=2.5, plot=True)
+        # sim.position_control_twist1(position=2.8, duration=2.5, plot=True)
+        # sim.position_control_contraction(duration=2.5, plot=True)
+        # sim.position_control_twist2(position=2.8, duration=2.5, plot=True)
+        # sim.position_control_contraction(duration=2.5, plot=True)
     except KeyboardInterrupt:
         print("Simulation interrupted by user.")
     finally:
         times, dists = zip(*sim.distances)
-        plt.title("Vertical Twisting Mode: Locked")
+        plt.title("Vertical Crawl Mode: Locked")
         plt.grid(True)
         plt.plot(times, dists)
         plt.xlabel("Time (s)")
