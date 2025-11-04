b393ec9e1dec4a0fc1a9c5660ee6de5c390627d7
rdlynx19 <pushkardave.vnit@gmail.com>
2025-11-04 17:31:08 -0600

    added truncation based on time limit

--- rl_scripts/test_env.py
+++ rl_scripts/test_env.py
@@ -1,7 +1,9 @@
 import numpy as np
 from hsa_gym.envs.hsa_v1 import HSAEnv
+from gymnasium.wrappers import TimeLimit
 
 env = HSAEnv(render_mode="human", randomize_goal=False)
+env = TimeLimit(env, max_episode_steps=500)
 
 obs, info = env.reset()
 print("Initial Observation:", obs.shape)
