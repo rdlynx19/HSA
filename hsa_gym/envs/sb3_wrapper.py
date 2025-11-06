288cf51b35ec3a64bc929efb8e9191f1fd0de626
rdlynx19 <pushkardave.vnit@gmail.com>
2025-11-05 21:42:59 -0600

    added locks handling in sb3 wrapper

--- hsa_gym/envs/sb3_wrapper.py
+++ hsa_gym/envs/sb3_wrapper.py
@@ -9,23 +9,39 @@ class SB3Wrapper(gym.ActionWrapper):
         motors_low = self.env.action_space['motors'].low
         motors_high = self.env.action_space['motors'].high
 
-        locks_shape = self.env.action_space['locks'].shape
-        locks_low = np.zeros(locks_shape, dtype=np.float32)
-        locks_high = np.ones(locks_shape, dtype=np.float32)
+        self.has_locks = "locks" in self.env.action_space.spaces
+        if self.has_locks:
+            locks_shape = self.env.action_space['locks'].shape
+            locks_low = np.zeros(locks_shape, dtype=np.float32)
+            locks_high = np.ones(locks_shape, dtype=np.float32)
+
+            low = np.concatenate([motors_low, locks_low])
+            high = np.concatenate([motors_high, locks_high])
+        else:
+            low = motors_low
+            high = motors_high
 
         self.action_space = gym.spaces.Box(
-            low=np.concatenate([motors_low, locks_low]),
-            high=np.concatenate([motors_high, locks_high]),
+            low=low,
+            high=high,
             dtype=np.float32
         )
 
+        # Cache size for action splitting
+        self.motors_size = motors_low.shape[0]
+        self.locks_size = locks_low.shape[0] if self.has_locks else 0
+
     def action(self, action):
         # Split flat action back into dict format
-        motors_len = self.env.action_space['motors'].shape[0]
+        motors_len = self.motors_size
         motors_act = action[:motors_len]
-        locks_act = np.round(action[motors_len:]).astype(np.int8)
-
-        return {
-            'motors': motors_act,
-            'locks': locks_act
-        }
+        if self.has_locks:
+            locks_act = np.round(action[motors_len:]).astype(np.int8)
+            return {
+                'motors': motors_act,
+                'locks': locks_act
+            }
+        else:
+            return {
+                'motors': motors_act
+            }
