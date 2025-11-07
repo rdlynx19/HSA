b809d434dc11b38b82ef81f3da0494b211296614
rdlynx19 <pushkardave.vnit@gmail.com>
2025-11-07 15:30:14 -0600

    corrected env creation in eval script

--- rl_scripts/ppo_eval.py
+++ rl_scripts/ppo_eval.py
@@ -2,26 +2,28 @@ import gymnasium as gym
 import numpy as np
 
 from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv, VecMonitor
 
-from hsa_gym.envs.hsa_v1 import HSAEnv
-from hsa_gym.envs.sb3_wrapper import SB3Wrapper
+from gymnasium.wrappers import TimeLimit
+
+from hsa_gym.envs.hsa_v2 import HSAEnv
 
 
 def make_env():
     """
     Helper function to create the environment
     """
-    env = HSAEnv(render_mode="human", randomize_goal=False, use_locks=False)
-    env = gym.wrappers.TimeLimit(env, max_episode_steps=500)
-    env = SB3Wrapper(env)
+    env = HSAEnv(render_mode="human", actuator_groups=[1])
+    env = TimeLimit(env, max_episode_steps=2000)
     return env
+
 
 def main():
     # Create the environment
     env = make_env()
 
     # Load the pre-trained PPO model
-    model = PPO.load("ppo_lamb1_final", env=env)
+    model = PPO.load("ppo_lamb2_randomMove", env=env)
 
     num_episodes = 15
     for ep in range(num_episodes):
